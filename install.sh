60f590a15ee79260e3701fe3c9d18398398b76b2
Anup Kishore <anup.kishore@gmail.com>
2024-10-15 21:39:57 -0400

    WIP -- fix install script

--- install.sh
+++ install.sh
@@ -22,7 +22,7 @@ Darwin*)
 	echo "  -> Installing .purepower..." && (cd && curl -fsSLO https://raw.githubusercontent.com/romkatv/dotfiles-public/master/.purepower)
 	echo "Swapping to ZSH!" && eval -l "$(grep /zsh$ /etc/shells | tail -1)"
 
-	echo "-> Installing latest vim..." && (brew install vim)
+	echo "-> Installing latest vim and nvim..." && (brew install vim neovim)
 	echo "-> Installing vim-plug..." && curl -fLosS ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
 	echo "-> Installing gnu-sed..." && (brew install gnu-sed)
@@ -39,8 +39,25 @@ Darwin*)
 	echo "-> Installing shfmt + shellcheck..." && (brew install shfmt shellcheck)
 	unset HOMEBREW_NO_AUTO_UPDATE
 	;;
-# Linux*)     machine=Linux;;
-# CYGWIN*)    machine=Cygwin;;
-# MINGW*)     machine=MinGw;;
+Linux*)
+	# TODO: Add -y to apt commands, reorganize
+	echo "Updating apt" && (sudo apt update)
+	echo "Installing git,zsh,tmux" && (sudo apt install git zsh tmux)
+	echo "Changing to zsh" && (chsh -s `which zsh`)
+
+	echo "-> Installing homeshick..." && (git clone git://github.com/andsens/homeshick.git "$HOME/.homesick/repos/homeshick")
+	"$HOME/.homesick/repos/homeshick/bin/homeshick" clone anup-the-magic/castle
+
+	echo "-> Installing oh-my-zsh..." && sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)" "" "--keep-zshrc"
+
+	echo "-> Installing tmux plugin manager..." && (git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm)
+	echo "  -> Manually triggering a plugin install" && (~/.tmux/plugins/tpm/bindings/install_plugins)
+	echo "-> Installing vim/nvim" && (sudo apt install vim neovim)
+
+	echo "-> Installing ripgrep" && (sudo apt install ripgrep)
+	echo "-> Installing fzf" && (sudo apt install fzf)
+	;;
+# CYGWIN*)	machine=Cygwin;;
+# MINGW*)	 machine=MinGw;;
 *) echo "Unable to detect OS Version: ($unameOut) is not set up for this install script" >&2 && exit 1 ;;
 esac
--- steps.md
+++ steps.md
@@ -1,23 +1,24 @@
--   install oh-my-zsh
--   install powerlevel10k [manual]
-    -   symlink powerlevel10k to ~/.oh-my-zsh/custom/themes/
--   install nvim
--   install homeshick
--   symlink
--   installs:
+- install oh-my-zsh
+- install powerlevel10k [manual]
+  - symlink powerlevel10k to ~/.oh-my-zsh/custom/themes/
+- install nvim
+- install homeshick
+- symlink
+- installs:
 
-    -   tig
-    -   rg [ripgrep]
-    -   jq, gron
-    -   asdf [via git]
-        -   LTS_VERSION=[get LTS version from node site, or ]
-        -   asdf plugin add nodejs
-        -   asdf install nodejs $LTS_VERSION
-        -   asdf global nodejs $LTS_VERSION
-    -   npm install -g prettier
-    -   tmux-plugin-manager
-    -   powerline?
+  - tig
+  - rg [ripgrep]
+  - jq, gron
+  - asdf [via git]
+    - LTS_VERSION=[get LTS version from node site, or ]
+    - asdf plugin add nodejs
+    - asdf install nodejs $LTS_VERSION
+    - asdf global nodejs $LTS_VERSION
+  - npm install -g prettier
+  - tmux-plugin-manager
+    - Also run the "install plugins" command!
+  - powerline?
 
--   Manual: copy ~/company folder?
+- Manual: copy ~/company folder?
 
-    -   need to figure out process for this
+  - need to figure out process for this
