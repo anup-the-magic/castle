c0772a5c008de7acf4c1339e10d437eabdc8afef
Anup Kishore <anup.kishore@gmail.com>
2025-11-06 16:09:49 -0800

    Add tree.dir to watch a directory, and remote swapping commands for WSL

--- home/.oh-my-zsh/custom/2-aliases.zsh
+++ home/.oh-my-zsh/custom/2-aliases.zsh
@@ -10,3 +10,18 @@
 (( $+commands[nvim] )) && {
   alias nvim.old='NVIM_APPNAME="nvim.old" nvim'
 }
+
+tree.dir () {
+  if (( $+commands[fd] )) ; then
+    fd=fd
+  else
+    fd=git ls-files
+  fi
+
+  treecmd="tree $@ --fromfile"
+  $fd | entr -dcrs "$fd | $treecmd"
+}
+
+alias swap-to-win='cd $(git get-remote-dir windows)'
+alias swap-to-wsl='cd $(git get-remote-dir wsl)'
+alias swap-dirs='cd $(git get-remote-dir $(git remote | grep -v origin))'
