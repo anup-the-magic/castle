e55fb405b6707b69de0348bc8a21fb46ae77578e
Anup Kishore <anup@compoundeye.com>
2025-11-17 12:05:00 -0800

    Add hostname to left side for more clear access

--- home/.p10k.zsh
+++ home/.p10k.zsh
@@ -35,6 +35,7 @@
     wslmode                 # see prompt_wslmode
     # os_icon               # os identifier
     dir                     # current directory
+    context                 # user@hostname
     vcs                     # git status
     # =========================[ Line #2 ]=========================
     newline                 # \n
@@ -84,7 +85,6 @@
     azure                   # azure account name (https://docs.microsoft.com/en-us/cli/azure)
     gcloud                  # google cloud cli account and project (https://cloud.google.com/)
     google_app_cred         # google application credentials (https://cloud.google.com/docs/authentication/production)
-    context                 # user@hostname
     nordvpn                 # nordvpn connection status, linux only (https://nordvpn.com/)
     ranger                  # ranger shell (https://github.com/ranger/ranger)
     nnn                     # nnn shell (https://github.com/jarun/nnn)
